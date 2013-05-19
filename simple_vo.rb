38408ff52fc7a2111901994cb96bd62c91fb6d03
mgarces <mgarces@csrg.inf.utfsm.cl>
2013-05-18 20:15:01 -0400

    migration to github

--- simple_vo.rb
+++ simple_vo.rb
@@ -7,6 +7,7 @@ class SimpleVO < Sinatra::Base
   
   get '/search' do
     votable = RestClient.get 'https://almascience.nrao.edu/aq/search?source_name_sesame=m87&radius=0:10:00&scan_intent-asu==*TARGET*&viewFormat=asdm&download=true'
+    
     content_type 'text/xml'
     votable
   end
