fb82cbcf86bd6fdfe3c64aaed1d9caa36eba1eee
mgarces <mgarces@csrg.inf.utfsm.cl>
2013-06-11 04:09:17 -0400

    cleaning test files

--- spec/voApp_spec.rb
+++ spec/voApp_spec.rb
@@ -11,13 +11,4 @@ describe 'VOApp' do
     get '/'
     last_response.should be_ok
   end
-  
-  it "should have right content in home page" do
-    get '/'
-    last_response.body.should == 'Dummy Virtual Observatory'
-  end
-  
-  # it 'should pass the test' do
- #    1.should == 1
- #  end
 end
